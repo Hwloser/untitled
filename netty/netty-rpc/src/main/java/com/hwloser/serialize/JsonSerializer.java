b3889b4fc4b11438f6c08be41643ca697c0f7093
hwloser <hwloser@163.com>
2021-05-16 12:11:59 +0800

    1. 提供 对于 json 的序列化实现

---
+++ netty/netty-rpc/src/main/java/com/hwloser/serialize/JsonSerializer.java
@@ -0,0 +1,15 @@
+package com.hwloser.serialize;
+
+import com.alibaba.fastjson.JSON;
+
+public class JsonSerializer implements Serializer {
+
+  @Override
+  public byte[] serialize(Object object) {
+    return JSON.toJSONBytes(object);
+  }
+  @Override
+  public <T> T deserialize(Class<T> clazz, byte[] bytes) {
+    return JSON.parseObject(bytes, clazz);
+  }
+}
