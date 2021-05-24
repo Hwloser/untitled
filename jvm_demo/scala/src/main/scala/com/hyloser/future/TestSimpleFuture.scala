89df014b9e8cb7d9f1819b4df0c78e651f45cc2f
hwloser <hwloser@163.com>
2021-05-24 16:42:42 +0800

    1. add test for future

--- jvm_demo/scala/src/main/scala/com/hyloser/future/TestSimpleFuture.scala
+++ jvm_demo/scala/src/main/scala/com/hyloser/future/TestSimpleFuture.scala
@@ -2,32 +2,98 @@ package com.hyloser.future
 
 import com.hyloser.TestUtils
 
-import java.util.concurrent.{Executors => JExecutors}
+import java.util.concurrent.{TimeUnit, Executors => JExecutors}
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
-import scala.util.{Failure, Success}
+import scala.concurrent.{Await, ExecutionContextExecutorService, Future, Promise}
+import scala.util.{Failure, Success, Try}
 
 object TestSimpleFuture {
   def main(args: Array[String]): Unit = {
     simpleFutureDefine()
     testJExecutionContext()
     testScalaFuture()
-    testScalaFutureError()
+    Try(testScalaFutureError())
+    testScalaPromise
+    testPromise
+  }
+
+  private def testPromise = {
+    Try {
+      TestUtils.setFunctionHeader("testPromise failure try")
+//      import scala.concurrent.ExecutionContext.Implicits.global
+      val promise = Promise[Int]()
+
+      val a = promise success 1
+      val b = promise failure new IllegalArgumentException("error")
+
+      println(a)
+      println(b)
+    } match {
+      case Failure(exception) => println(exception.getMessage)
+    }
+
+    Try {
+
+    }
+
+
+  }
+
+  private def testScalaPromise = {
+    TestUtils.setFunctionHeader("testScalaPromise")
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    // define a promise of type int
+    val promise = Promise[Int]()
+
+    // define a future from promise
+    val future = promise.future
+
+    future.onComplete {
+      case Failure(exception) => println(s"future on failure, exception:${exception.getMessage}")
+      case Success(value) => println(s" future on success, value:$value")
+    }
+
+    promise.success(generateMessage("success validate"))
+    val failedPromise = Promise[Int]()
+    failedPromise.failure(Try(generateMessage("failure validate")).failed.get)
+
+    // completing promise use promise.complete method
+    val promise2 = Promise[Int]()
+    val future2 = promise.future
+
+    future2.onComplete {
+      case Success(stock) => println(s"Stock for vanilla donut = $stock")
+      case Failure(e) => println(s"Failed to find vanilla donut stock, exception = $e")
+    }
+
+    Await.result(future2, Duration.Inf)
+
+    promise2.complete(Try(generateMessage("failure validate")))
+  }
+
+  def generateMessage(donut: String): Int = {
+    if (donut == "success validate") 10
+    else throw new IllegalStateException("Out of stock")
   }
 
   private def testScalaFutureError() = {
-    TestUtils.setFunctionHeader("testFutureError")
-    import scala.concurrent.ExecutionContext.Implicits.global
-    val f = Future[Int] {
-      1 / 0
-    }
+    Try {
+      TestUtils.setFunctionHeader("testFutureError")
+      import scala.concurrent.ExecutionContext.Implicits.global
+      val f = Future[Int] {
+        1 / 0
+      }
 
-    f.onComplete {
-      case Failure(exception) => exception.printStackTrace()
-      case Success(value) => println(s"value: $value")
-    }
+      f.onComplete {
+        case Failure(exception) => exception.printStackTrace()
+        case Success(value) => println(s"value: $value")
+      }
 
-    Await.result(f, Duration(5, "m"))
+      Await.result(f, Duration(5, TimeUnit.MINUTES))
+    } match {
+      case Failure(e) => println(e.getMessage)
+    }
   }
 
   private def testScalaFuture() = {
@@ -47,11 +113,18 @@ object TestSimpleFuture {
     Await.result(sumFuture, Duration.Inf)
   }
 
+  private def customizeExecutionContextExecutorService: ExecutionContextExecutorService = {
+    TestUtils.setFunctionHeader("customizeExecutionContextExecutorService")
+    val jFixedExecutor = JExecutors.newSingleThreadExecutor()
+    return scala.concurrent.ExecutionContext
+      .fromExecutorService(jFixedExecutor)
+  }
 
   private def testJExecutionContext() = {
     TestUtils.setFunctionHeader("testJExecutionContext")
-    val jFixedExecutor = JExecutors.newSingleThreadExecutor()
-    val executors = scala.concurrent.ExecutionContext.fromExecutorService(jFixedExecutor)
+
+    val executors = customizeExecutionContextExecutorService
+
     val f = executors.submit(() => {
       var sum = 0
       for (i <- 0 until 100) sum += i
