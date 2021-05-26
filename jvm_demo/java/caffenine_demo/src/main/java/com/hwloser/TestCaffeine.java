2877c258f00714420b06ee0327da5f7ebbf080bd
hwloser <hwloser@163.com>
2021-05-26 15:58:12 +0800

    1. add caffeine library

--- jvm_demo/java/caffenine_demo/src/main/java/com/hwloser/TestCaffeine.java
+++ jvm_demo/java/caffenine_demo/src/main/java/com/hwloser/TestCaffeine.java
@@ -12,17 +12,39 @@ public class TestCaffeine {
 
     Cache<Object, Object> cache = Caffeine.newBuilder()
         .initialCapacity(128)
-//        .expireAfterWrite(2, TimeUnit.SECONDS)
+        .expireAfterWrite(2, TimeUnit.SECONDS)
         .build();
 
     for (int i = 0; i < 8; i++) {
-      Object c = cache.get("o", k -> k + "" + a.incrementAndGet());
+      Object c = cache.get(i, k -> k + "" + a.incrementAndGet());
       System.out.println(c);
 
       System.gc();
 
       Thread.sleep(TimeUnit.SECONDS.toMillis(1));
-    }
 
+      Object c1 = cache.get(i, k -> k + "" + a.incrementAndGet());
+      System.out.println(c1);
+
+      Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+    }
+  }
+
+  private void testWeakKeysMap() {
+    AtomicInteger a = new AtomicInteger();
+    Cache<Object, Object> cache = Caffeine.newBuilder()
+        .initialCapacity(128)
+        .weakKeys()
+        .expireAfterWrite(2, TimeUnit.SECONDS)
+        .build();
+
+    Object c1 = cache.get("i", k -> k + "" + a.incrementAndGet());
+
+    System.gc(); /* breakpoint gc, or trigger gc explicitly */
+
+    Object c2 = cache.get("i", k -> k + "" + a.incrementAndGet());
+
+    System.out.println(c1);
+    System.out.println(c2);
   }
 }
