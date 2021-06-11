80c45ad3013c4b738b4bc9c7c58dfec06a75be14
hwloser <hwloser@163.com>
2021-06-12 01:28:22 +0800

    1. add hadoop yarn client demo

--- jvm_demo/java/hadoop/src/main/java/com/hwloser/simple/YarnClientDemo.java
+++ jvm_demo/java/hadoop/src/main/java/com/hwloser/simple/YarnClientDemo.java
@@ -1,5 +1,27 @@
 package com.hwloser.simple;
 
+import java.io.IOException;
+import org.apache.hadoop.yarn.client.api.YarnClient;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.exceptions.YarnException;
+
 public class YarnClientDemo {
+
+  private static YarnClient yarnClient;
+
+  public static void main(String[] args) throws IOException, YarnException {
+    // 首先初始化和启动YarnClient
+    initYarnClient();
+
+    yarnClient.getLabelsToNodes().forEach(System.out::printf);
+  }
+
+  private static void initYarnClient() {
+    YarnConfiguration conf = new YarnConfiguration();
+    yarnClient = YarnClient.createYarnClient();
+    yarnClient.init(conf);
+    yarnClient.start();
+
+  }
 
 }
