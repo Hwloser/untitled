0fc5d4b0db3ea3c0ae72d829b69403c810d368b2
hwloser <hwloser@163.com>
2021-07-01 17:23:56 +0800

    add spark graphX pregel demo

--- spark-demo/src/main/java/com/hwloser/graph/TestPregel.scala
+++ spark-demo/src/main/java/com/hwloser/graph/TestPregel.scala
@@ -1,8 +1,29 @@
 package com.hwloser.graph
 
+import org.apache.spark.graphx.util.GraphGenerators
+import org.apache.spark.sql.SparkSession
+
 object TestPregel {
 
+  val spark = SparkSession
+    .builder()
+    .appName("test pregel demo")
+    .getOrCreate()
+
+  val sc = spark.sparkContext
+
   def main(args: Array[String]): Unit = {
+    val graph = GraphGenerators
+      .logNormalGraph(sc, 100)
+      .mapVertices((id, _) => id.toDouble)
+
+    println("println cached triplets")
+    graph
+      .cache()
+      .triplets
+      .collect()
+      .foreach(println)
+
 
   }
 }
